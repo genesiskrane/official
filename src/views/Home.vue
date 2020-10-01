ed41c236a59f85832a4b9cb2c5fae21b107976ff
sypher-samurai <spoiltcooker@gmail.com>
2020-10-01 10:39:28 +0100

    Putting Ready Made Template

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Hero />
+    <Gallery />
+    <LatestPost />
+  </div>
+</template>
+
+<script>
+import Hero from "../components/Hero";
+import Gallery from "../components/Gallery";
+import LatestPost from "../components/LatestPost";
+export default {
+  name: "Home",
+
+  data() {
+    return {
+      items: [],
+    };
+  },
+  components: {
+    Hero,
+    Gallery,
+    LatestPost,
+  },
+};
+</script>
+
+<style>
+</style>
