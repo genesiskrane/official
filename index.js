21248baa6ddade192398efe1a34186826b499870
augmentchinedu <augmentchinedu@gmail.com>
2020-10-01 13:36:26 +0100

    F

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ const path = require("path");
 const app = express();
 app.use(cors());
 app.use(morgan("dev"));
-app.use('/',
-  express.static(
-    path.join(path.dirname(require.main.filename), "client", "dist")
-  )
-);
+app.use(express.static(path.join(__dirname, "client", "dist")));
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server Running @ 3000");
 });
