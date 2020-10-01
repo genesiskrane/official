c4fd792a46fd6be7e413f68d53360eea7447514d
augmentchinedu <augmentchinedu@gmail.com>
2020-10-01 23:46:50 +0100

    Redirect Added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.get("/", (req, res) => {
+  res.redirect("/index.html")
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 app.use(express.static(path.join(__dirname, "client", "dist")));
