19ea70f825432d460f70fb4839c3060d7ae522fb
sypher-samurai <spoiltcooker@gmail.com>
2020-10-01 12:49:54 +0100

    Updating

--- client/src/components/Newsletter.vue
+++ client/src/components/Newsletter.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card color="blue-grey darken-1" dark :loading="isUpdating">
+  <v-card color="#4285f4" dark :loading="isUpdating">
     <template v-slot:progress>
       <v-progress-linear
         absolute
@@ -12,51 +12,27 @@
     <v-form>
       <v-container>
         <v-row>
-          <v-col cols="12">
-            <v-text-field
-              v-model="name"
-              :error-messages="nameErrors"
-              :counter="10"
-              label="Name"
-              required
-              @input="$v.name.$touch()"
-              @blur="$v.name.$touch()"
-            ></v-text-field>
-            <v-text-field
-              v-model="email"
-              :error-messages="emailErrors"
-              label="E-mail"
-              required
-              @input="$v.email.$touch()"
-              @blur="$v.email.$touch()"
-            ></v-text-field>
-            <v-select
-              v-model="select"
-              :items="items"
-              :error-messages="selectErrors"
-              label="Item"
-              required
-              @change="$v.select.$touch()"
-              @blur="$v.select.$touch()"
-            ></v-select>
-            <v-checkbox
-              v-model="checkbox"
-              :error-messages="checkboxErrors"
-              label="Do you agree?"
-              required
-              @change="$v.checkbox.$touch()"
-              @blur="$v.checkbox.$touch()"
-            ></v-checkbox>
+          <v-col cols="10">
+          <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
+      required
+    ></v-text-field>
           </v-col>
-        </v-row>
-        <v-row>
-            <v-col>
-
-                    <v-btn small elevation="2" color="red"> Vegeg</v-btn>
-                <v-spacer></v-spacer>
-                    <v-btn small elevation="2" color="red"> Vegeg</v-btn>
-                
-            </v-col>
+          <v-col cols="2">
+            <div class="px-5">
+              <v-btn
+                small
+                elevation="2"
+                color="#ea4436"
+                class="pa-6"
+                @click="validate()"
+              >
+                submit</v-btn
+              >
+            </div>
+          </v-col>
         </v-row>
       </v-container>
     </v-form>
@@ -66,45 +42,28 @@
 <script>
 export default {
   data() {
-    const srcs = {
-      1: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
-      2: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
-      3: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
-      4: "https://cdn.vuetifyjs.com/images/lists/4.jpg",
-      5: "https://cdn.vuetifyjs.com/images/lists/5.jpg",
-    };
-
+  
     return {
-      autoUpdate: true,
-      friends: ["Sandra Adams", "Britta Holt"],
-      isUpdating: false,
-      name: "Midnight Crew",
-      people: [
-        { header: "Group 1" },
-        { name: "Sandra Adams", group: "Group 1", avatar: srcs[1] },
-        { name: "Ali Connors", group: "Group 1", avatar: srcs[2] },
-        { name: "Trevor Hansen", group: "Group 1", avatar: srcs[3] },
-        { name: "Tucker Smith", group: "Group 1", avatar: srcs[2] },
-        { divider: true },
-        { header: "Group 2" },
-        { name: "Britta Holt", group: "Group 2", avatar: srcs[4] },
-        { name: "Jane Smith ", group: "Group 2", avatar: srcs[5] },
-        { name: "John Smith", group: "Group 2", avatar: srcs[1] },
-        { name: "Sandra Williams", group: "Group 2", avatar: srcs[3] },
+      valid: true,
+      emil: "",
+      emailRules: [
+        (v) => !!v || "E-mail is required",
+        (v) =>
+          /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
+          "E-mail must be valid",
       ],
-      title: "The summer breeze",
+     
+      
+      
+      
+  
     };
-  },
-
-  watch: {
-    isUpdating(val) {
-      if (val) {
-        setTimeout(() => (this.isUpdating = false), 3000);
-      }
-    },
   },
 
   methods: {
+    validate() {
+      this.$refs.form.validate();
+    },
     remove(item) {
       const index = this.friends.indexOf(item.name);
       if (index >= 0) this.friends.splice(index, 1);
