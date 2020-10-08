f8486d3b9842a5709883a9cdddb601f35efa639d
sypher-samurai <spoiltcooker@gmail.com>
2020-10-08 21:08:20 +0100

    Progress

--- client/src/main.js
+++ client/src/main.js
@@ -1,45 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
-// import VueRouter from 'vue-router';
+
+imp
 
 
-
-
-// import Home from './views/Home.vue';
-// import About from './views/About.vue';
-// import Contact from './views/Contact.vue';
-// import OptOut from './views/OptOut.vue'
-// import NotFound from './views/NotFound.vue';
-
-// Vue.use(VueRouter);
-
-import '../src/scss/main.scss'
 Vue.config.productionTip = false
-
-// const router = new VueRouter({
-//   mode:"history",
-//   routes: [
-//     {
-//       path: '',component: Home 
-//     },
-//     {
-//       path: '#about',component: About
-//     },
-//     {
-//       path: '/contact',component: Contact 
-//     },
-//     {
-//       path:'/opt-out-newsletter', component: OptOut
-//     },
-//     {
-//       path: '*',component: NotFound 
-//     }
-//   ]
-// })
-
 new Vue({
-  // router,
-  vuetify,
+
+
   render: h => h(App)
 }).$mount('#app')
