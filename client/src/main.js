cf96f2ad43bd0796b6c9147e8d9209d7c2998363
sypher-samurai <spoiltcooker@gmail.com>
2020-10-09 13:37:47 +0100

    html files deleted and css full added

--- client/src/main.js
+++ client/src/main.js
@@ -7,6 +7,14 @@ import '../src/assets/css/font-awesome/css/font-awesome.css'
 import '../src/assets/css/bootstrap.min.css'
 import '../src/assets/css/pe-icon-7-stroke/css/pe-icon-7-stroke.css'
 import '../src/assets/css/style.css'
+import '../src/assets/js/revolution/css/layers.css'
+import '../src/assets/js/revolution/css/navigation.css'
+import '../src/assets/js/revolution/css/settings.css'
+import '../src/assets/js/revolution/css/settings2.css'
+import '../src/assets/js/revolution/css/settings3.css'
+import '../src/assets/js/vendors/owl-carousel/owl.carousel.css'
+import '../src/assets/js/vendors/owl-carousel/owl.theme.css'
+import '../src/assets/js/vendors/owl-carousel/owl.transitions.css'
 Vue.config.productionTip = false
 new Vue({
 
