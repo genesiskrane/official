b45df62f3074c055cc95d4f8d740208a7fb3ad66
sypher-samurai <spoiltcooker@gmail.com>
2020-10-09 10:06:15 +0100

    Styles added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,13 @@
 <template>
-  <v-app>
-    <v-app-bar app dense flat>
-      <v-img
-        lazy-src="./assets/logo.png"
-        max-height="150"
-        max-width="250"
-        src="./assets/logo.png"
-      ></v-img>
-    </v-app-bar>
-    <v-main>
-      <v-container fluid>
-        Hello World
-      </v-container>
-    </v-main>
-    <v-footer app></v-footer>
-    <v-bottom-navigation app></v-bottom-navigation>
-  </v-app>
+  <h1>Hello</h1>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-imp
 
 
+import '../src/assets/css/font-awesome/css/font-awesome.css'
+import '../src/assets/css/bootstrap.min.css'
+import '../src/assets/css/pe-icon-7-stroke/css/pe-icon-7-stroke.css'
+import '../src/assets/css/style.css'
 Vue.config.productionTip = false
 new Vue({
 
